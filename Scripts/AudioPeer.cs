2cae22013e436aadbc4a2f4cbfa928166cc26ccf
KyleToohey <kyletoohey1@yahoo.com>
2017-11-04 13:29:04 -0400

    Add files via upload

---
+++ Scripts/AudioPeer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioPeer : MonoBehaviour {
+    AudioSource _audioSource;
+    public float[] _samples = new float[64];
+	// Use this for initialization
+	void Start () {
+        _audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetSpectrumAudioSource();
+	}
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+}
